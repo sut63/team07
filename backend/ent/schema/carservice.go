2260c32eb508d8ddea217a424dc3eddeaa1ea4d0
Toneone11 <58927899+Toneone11@users.noreply.github.com>
2020-12-15 20:26:25 +0700

    Create carservice.go

---
+++ backend/ent/schema/carservice.go
@@ -0,0 +1,18 @@
+package schema
+
+import "github.com/facebookincubator/ent"
+
+// CarService holds the schema definition for the CarService entity.
+type CarService struct {
+	ent.Schema
+}
+
+// Fields of the CarService.
+func (CCarService) Fields() []ent.Field {
+	return nil
+}
+
+// Edges of the CarService.
+func (CarService) Edges() []ent.Edge {
+	return nil
+}
