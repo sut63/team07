09f1ae3d689c0e445b6c980535ceefba76b7749c
Toneone11 <58927899+Toneone11@users.noreply.github.com>
2020-12-24 20:04:33 +0700

    ทำ Entity สำหรับเก็บข้อมูลให้เสร็จ - close #47

---
+++ backend/ent/schema/servicetype.go
@@ -0,0 +1,22 @@
+package schema
+import (
+ "github.com/facebookincubator/ent"
+ "github.com/facebookincubator/ent/schema/field"
+ "github.com/facebookincubator/ent/schema/edge"
+)
+// Servicetype holds the schema definition for the Servicetype entity.
+type Servicetype struct {
+ ent.Schema
+}
+// Fields of the Servicetype.
+func (Servicetype) Fields() []ent.Field {
+ return []ent.Field{
+ field.String("servicetype").NotEmpty(),
+ }
+}
+// Edges of the Servicetype.
+func (Servicetype) Edges() []ent.Edge {
+ return []ent.Edge{
+    edge.To("typeid",Carservice.Type),
+ }
+}
