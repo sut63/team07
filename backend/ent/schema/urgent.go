722c7176ded47986b69ddf1bb99f5c334cca9f25
Toneone11 <58927899+Toneone11@users.noreply.github.com>
2020-12-27 15:34:55 +0700

    ทำ Entity สำหรับเก็บข้อมูลให้เสร็จ - close #47

--- backend/ent/schema/urgent.go
+++ backend/ent/schema/urgent.go
@@ -1,18 +1,22 @@
 package schema
-
-import "github.com/facebookincubator/ent"
-
+import (
+ "github.com/facebookincubator/ent"
+ "github.com/facebookincubator/ent/schema/field"
+ "github.com/facebookincubator/ent/schema/edge"
+)
 // Urgent holds the schema definition for the Urgent entity.
 type Urgent struct {
-	ent.Schema
+ ent.Schema
 }
-
 // Fields of the Urgent.
 func (Urgent) Fields() []ent.Field {
-	return nil
+ return []ent.Field{
+ field.String("urgent").NotEmpty(),
+ }
 }
-
 // Edges of the Urgent.
 func (Urgent) Edges() []ent.Edge {
-	return nil
+ return []ent.Edge{
+    edge.To("urgentid",Carservice.Type),
+ }
 }
