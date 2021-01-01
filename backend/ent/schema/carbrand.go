817dc71956f1aea20f1fcb3b9c22e3eede13f404
jxxshark <b6112745@g.sut.ac.th>
2021-01-01 16:16:59 +0700

    ส้ราง entity สำหรับระบบลงทะเบียนรถ _ close #70

---
+++ backend/ent/schema/carbrand.go
@@ -0,0 +1,27 @@
+package schema
+
+import (
+	"github.com/facebookincubator/ent"
+	"github.com/facebookincubator/ent/schema/edge"
+	"github.com/facebookincubator/ent/schema/field"
+)
+// Carbrand holds the schema definition for the Carbrand entity.
+type Carbrand struct {
+	ent.Schema
+}
+
+// Fields of the Carbrand.
+func (Carbrand) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("brand").
+		Unique(),
+	}
+}
+
+// Edges of the Carbrand.
+func (Carbrand) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.To("brandof",Ambulance.Type).
+		StorageKey(edge.Column("brand_id")),
+	}
+}
---
+++ backend/ent/schema/insurance.go
@@ -0,0 +1,29 @@
+package schema
+
+import (
+	"github.com/facebookincubator/ent"
+	"github.com/facebookincubator/ent/schema/edge"
+	"github.com/facebookincubator/ent/schema/field"
+)
+// Insurance holds the schema definition for the Insurance entity.
+type Insurance struct {
+	ent.Schema
+}
+
+// Fields of the Insurance.
+func (Insurance) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("classofinsurance").
+		Unique(),
+		field.String("company").
+		Unique(),
+	}
+}
+
+// Edges of the Insurance.
+func (Insurance) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.To("insuranceof",Ambulance.Type).
+		StorageKey(edge.Column("insurance_id")),
+	}
+}
