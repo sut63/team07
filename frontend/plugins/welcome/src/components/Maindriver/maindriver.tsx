3bde22f1b902c792130de80e51ce40112ad28d2b
Haiserest <69398619+Haiserest@users.noreply.github.com>
2021-01-11 18:12:22 +0700

    new UI ระบบเข้าออก -close #149

--- frontend/plugins/welcome/src/components/Maindriver/maindriver.tsx
+++ frontend/plugins/welcome/src/components/Maindriver/maindriver.tsx
@@ -2,14 +2,17 @@ import React, { FC } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
-
+import { jaikeleBase64Function } from '../../image/jaikele'
+import CardMedia from '@material-ui/core/CardMedia';
+import FormControl from '@material-ui/core/FormControl';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import {
  Content,
  Header,
  Page,
  pageTheme,
  ContentHeader,
- Link,
+ Link
 } from '@backstage/core';
 
 const MainDriverPage: FC<{}> = () => {
@@ -17,9 +20,32 @@ const profile = { givenName: 'ระบบรถพยาบาล' };
 const HeaderCustom = {
     minHeight: '50px',
   };
-  
+  const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+      root: {
+          display: 'flex',
+          flexWrap: 'wrap',
+          justifyContent: 'center',
+      },
+      margin: {
+          margin: theme.spacing(3),
+      },
+      withoutLabel: {
+          marginTop: theme.spacing(3),
+      },
+      textField: {
+          width: '25ch',
+      },
+      media: {
+          height: 0,
+          marginLeft: 25,
+          maxWidth: 300,
+      }
+      
+  }),
+);
+const classes = useStyles();  
  return (
-    
     <Page theme={pageTheme.other} >
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
      <Header
@@ -32,11 +58,10 @@ const HeaderCustom = {
      <Content>
        <ContentHeader title="">
        </ContentHeader>
-       <br></br>
-       <br></br>
-       <br></br>
-       <br></br>
        <center>
+       <br></br>
+       <br></br>
+       
        <ButtonGroup
         orientation="vertical"
         color="primary"
@@ -53,6 +78,16 @@ const HeaderCustom = {
         <Button><h1>ส่งผู้ป่วย</h1></Button>
         </Link>
       </ButtonGroup>
+      <br></br>
+      <FormControl
+        className={classes.media}>
+       <CardMedia
+          component="video"
+          src={jaikeleBase64Function}
+          style={{ marginTop: 30 }}
+          autoPlay
+       />
+       </FormControl>
        </center>
 
      </Content>
